469ff1e6d0bff16b3ce7d38a43614e1ed61f83bd
hugaojanuario <hugojanuario553@gmail.com>
2026-09-06 20:58:39 -0300

    feat: add raw stock data ingestion

--- src/ingestion/extract_stock.py
+++ src/ingestion/extract_stock.py
@@ -5,41 +5,60 @@ from zoneinfo import ZoneInfo
 
 import requests
 
-tickers = ["PETR4", "VALE3"]
+def fetch_stock(ticker):
+    response = requests.get(f'https://brapi.dev/api/v2/stocks/quote?symbols={ticker}', timeout=10)
+    response.raise_for_status()
 
-for ticker in tickers:
+    data = response.json()
 
-
-    try:
-        response = requests.get(f'https://brapi.dev/api/v2/stocks/quote?symbols={ticker}', timeout=10)
-        response.raise_for_status()
-        date = datetime.now(ZoneInfo("America/Sao_Paulo"))
-        year = date.year
-        month = date.strftime("%m")
-        day = date.strftime("%d")
-
-        data = response.json()
-
-        print(data)
-        print(response.status_code)
-
-        name_archive = date.strftime("%Y%m%dT%H%M%S%z") + ".json"
-        file_path = (
-            Path("data/raw/stocks")
-            / ticker
-            / str(year)
-            / month
-            / day
-            / name_archive
-        )
-        file_path.parent.mkdir(parents=True, exist_ok=True)
+    return data
 
 
-        with open(file_path, 'w', encoding='utf-8') as archive:
-            json.dump(data, archive, indent=4, ensure_ascii=False)
+def build_file_path(ticker):
+    date = datetime.now(ZoneInfo("America/Sao_Paulo"))
+    year = date.year
+    month = date.strftime("%m")
+    day = date.strftime("%d")
+
+    name_archive = date.strftime("%Y%m%dT%H%M%S%z") + ".json"
+    file_path = (
+        Path("data/raw/stocks")
+        / ticker
+        / str(year)
+        / month
+        / day
+        / name_archive
+    )
+    return file_path
 
 
-    except requests.exceptions.Timeout:
-        print("A requisição expirou.")
-    except requests.exceptions.RequestException as e:
-        print(f"Ocorreu um erro: {e}")
+def save_json(data, file_path):
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
+
+    with open(file_path, 'w', encoding='utf-8') as archive:
+        json.dump(data, archive, indent=4, ensure_ascii=False)
+
+
+
+def main ():
+    tickers = ["PETR4", "VALE3"]
+
+    for ticker in tickers:
+
+        try:
+            data = fetch_stock(ticker)
+            file_path = build_file_path(ticker)
+            save_json(data,file_path)
+
+            print (f"Arquivo em: {file_path}")
+
+        except requests.exceptions.Timeout:
+            print("A requisição expirou.")
+        except requests.exceptions.RequestException as e:
+            print(f"Ocorreu um erro: {e}")
+
+    pass
+
+if __name__ == "__main__":
+    main()
